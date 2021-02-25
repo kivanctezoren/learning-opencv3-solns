e073514b51e5be46256647957f4a6a4f1c364e9c
kivanctezoren <kivanctezoren@gmail.com>
2021-02-26 01:16:06 +0300

    Replace placeholder solutions

--- ocv4/ch2/ch2ex3/src/ch2ex3-ocv4.cpp
+++ ocv4/ch2/ch2ex3/src/ch2ex3-ocv4.cpp
@@ -1,41 +1,93 @@
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/core/utility.hpp"
+
 #include <iostream>
+#include <vector>
+#include <string>
+#include <ctime>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
 
 int main(int argc, char **argv) {
-    cv::namedWindow("ch2exp3-ocv4", cv::WINDOW_AUTOSIZE);
-    cv::VideoCapture vcap;
     cv::Mat frame;
+    cv::VideoCapture vcap;
     
-    // Note: The new string instance is not needed, argv[1] is a string and it
-    //  is passed by const reference
-    vcap.open(std::string(argv[1]));
+    string keys = "{@fps|60|captured frames per second}{@cam|0|camera id}";
+    cv::CommandLineParser parser(argc, argv, keys);
+    int cam = parser.get<int>("@cam");
+    int wait_time = 1000 / parser.get<int>("@fps");
+    
+    if (argc <= 3) {
+        vcap.open(cam);
+    } else {
+        cout << "Usage: " << argv[0] << " [FPS] [CAMERA_ID]" << endl;
+    }
+    
+    if (!vcap.isOpened()) {
+        cout << "Could not open camera." << endl;
+        return -1;
+    }
+    
+    cout << "Press Z to capture and save a downsampled color image." << endl;
+    cout << "Press ESC to quit the program." << endl;
+    
+    cv::namedWindow("ch2ex3-ocv3");
     
     while (true) {
         vcap >> frame;
-        
         if (frame.empty()) break;  // Reached end of video
         
-        cv::imshow("ch2exp3-ocv4", frame);
+        cv::imshow("ch2ex3-ocv3", frame);
         
-        // If no keys pressed, waitKey returns -1. My system recognizes this as
-        //  255, probably as a result of char overflow.
-        int keycode = cv::waitKey(33) & 255;
-        
-        if (keycode) {
-            keycode %= 255;  // Converting 255 to 0 & masking extraneous bits
-        } else {  // Keycode 0 should break (?)
-            std::cout << "Received keycode: " << keycode << " (" << (char) keycode << ") - Exiting." << std::endl;
+        switch (cv::waitKey(wait_time)) {
+        case 'z':
+        case 'Z':
+        {
+            char time_c_str[20];
+            string filename;
+            time_t raw_time;
+            struct tm *now;
+            
+            raw_time = time(nullptr);
+            now = localtime(&raw_time);
+            
+            strftime(time_c_str, 20, "%F-%H-%M-%S", now);
+            
+            filename = "capture-" + string(time_c_str) + ".png";
+            
+            // TODO: Should check if file exists, therefore allow multiple saves
+            //  in one second
+            
+            cv::Mat frame_sdown;  // Scaled down frame
+            cv::pyrDown(frame, frame_sdown);
+            
+            std::vector<int> png_params;
+            png_params.push_back(cv::IMWRITE_PNG_COMPRESSION);
+            png_params.push_back(3);
+            
+            bool success = false;
+            try {
+                success = cv::imwrite(filename, frame_sdown, png_params);
+            } catch (const cv::Exception& ex) {
+                cout << "Couldn't convert the captured image to PNG." << endl;
+            }
+            
+            if (success) {
+                cout << "PNG image saved: " << filename << endl;
+            } else {
+                cout << "Couldn't write the image." << endl;
+            }
+            
             break;
         }
-        
-        if (keycode > 0) {  // if keycode was not -1 (or 255)
-            std::cout << "Received keycode: " << keycode << " (" << (char) keycode << ") - Exiting." << std::endl;
-            break;
+        case 27:
+            return 0;
         }
     }
     
-    cv::destroyWindow("ch2exp3-ocv4");
-    
-    return 0;
+    return -1;
 }
